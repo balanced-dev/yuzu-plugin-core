d7cba622b9341fd2ff79691436f5662537e45c00
philwhittaker <phil@hifi.agency>
2023-01-11 11:04:23 +0000

    insert plugin folders before base

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const utils = require('./_dev/_source/js/utils');
 module.exports = {
     utils: utils,
     initForYuzuLoader: (config) => {
-        config.renderedPartialDirs.push('./node_modules/yuzu-plugin-core/_dev/_templates/_dataStructures');
+        config.renderedPartialDirs.splice(0, 0, './node_modules/yuzu-plugin-core/_dev/_templates/_dataStructures');
     },
     initForYuzuApi: (config) => {
-        config.api.files.templates.push('./node_modules/yuzu-plugin-core/_dev/_templates');
+        config.api.files.templates.splice(0, 0, './node_modules/yuzu-plugin-core/_dev/_templates');
     }
 };
